5e7a754dfcea6dedb73979dbf3fa8c38ed4b84cc
EsprIx <3sprix.exe@gmail.com>
2022-04-02 21:56:30 +0200

    created youtube url endpoint

--- server.py
+++ server.py
@@ -13,5 +13,14 @@ def getUserStats():
     return data
 
 
+@app.route(f"{BASE_ROUTE}/getYoutubeUrl", methods=["POST"])
+def getYoutubeUrl():
+    data = request.get_json()
+
+    url = f"https://youtube.com/{data['genre']}"
+
+    return {"url": url}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- utils/getMusics.py
+++ utils/getMusics.py
@@ -1,6 +1,28 @@
+import requests as req
+import random
+
+BASE_URL = "http://api.musixmatch.com/ws/1.1/"
+API_KEY = "c82cd9b49b81240563733e813a4a0198"
+
+
+def getEndpoint(method, args):
+    return BASE_URL + method + "?format=json&callback=callback&" + args + "&apikey=" + API_KEY
+
+
 def getMusicInfos(genre):
-    pass
-    return name, id
+    rdnItem = random.randint(0, 100)
+    endpoint = getEndpoint(
+        "track.search", f"f_has_lyrics=1&f_music_genre_name={genre}&page_size=100&page=1&s_track_rating=asc"
+    )
+    r = req.get(endpoint)
+    print(r.json()["message"]["body"]["track_list"][rdnItem])
+    # return name, id
+    # A FIX
+    
+    response = req.get("https://api.soundcloud.com/tracks?q=hello&ids=1,2,3&genres={genre}&access=playable&limit=3&linked_partitioning=true")
+
+
+getMusicInfos("JAZZ")
 
 
 def getMusicLyrics(id):

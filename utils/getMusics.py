69c949bba58b7da60e2579fdfb33653236fdb167
Kylobsi <aco.ced@orange.fr>
2022-04-02 21:57:08 +0200

    Update getMusics.py

--- utils/getMusics.py
+++ utils/getMusics.py
@@ -1,18 +1,26 @@
-def getMusicInfos(genre):
-    pass
-    return name, id
+import requests as req
+import random
+
+API_KEY = "AIzaSyAfHh4ljEXd3de6i5Daw5xG6mtlJYGkThE"
+BASE_URL = "https://youtube.googleapis.com/youtube/v3"
 
 
-def getMusicLyrics(id):
-    pass
-    return lyrics
+def getMusicInfos(genre) -> str:
+
+    r = req.get(
+        f"{BASE_URL}/search?maxResults=500&q={genre}%20instrumental&safeSearch=strict&type=video&key={API_KEY}")
+    r = r.json()
+
+    # print(r)
+
+    rnd = random.randint(0, len(r["items"])-1)
+    item = r["items"][rnd]
+    print(item)
+
+    videoID = item["id"]["videoId"]
+
+    videoURL = f"https://youtu.be/{videoID}?t=30"
+    return videoURL
 
 
-def findChorus(lyrics):
-    pass
-    return chorusTimeCode
-
-
-def getMusicUrl(name, chorusTimeCode):
-    pass
-    return url
+print("Le lien vers votre vidéo est :", getMusicInfos("lofi"))
